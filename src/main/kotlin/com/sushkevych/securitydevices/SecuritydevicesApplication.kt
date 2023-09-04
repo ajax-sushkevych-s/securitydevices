ebda1d4f06ce1d947e20b326adfe40743f9cb39b
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-09-04 10:39:09 +0300

    init

---
+++ src/main/kotlin/com/sushkevych/securitydevices/SecuritydevicesApplication.kt
@@ -0,0 +1,11 @@
+package com.sushkevych.securitydevices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecuritydevicesApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecuritydevicesApplication>(*args)
+}
---
+++ src/test/kotlin/com/sushkevych/securitydevices/SecuritydevicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sushkevych.securitydevices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecuritydevicesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
