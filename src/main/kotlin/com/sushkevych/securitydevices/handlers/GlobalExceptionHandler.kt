87365554a89349d5d7f4737739446897159d95ac
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-09-12 13:18:05 +0300

    PETPROJECT-00003 Migration to MongoDB Remove unused import

--- src/main/kotlin/com/sushkevych/securitydevices/handlers/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/sushkevych/securitydevices/handlers/GlobalExceptionHandler.kt
@@ -1,6 +1,5 @@
 package com.sushkevych.securitydevices.handlers
 
-import jakarta.persistence.EntityNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
