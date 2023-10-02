f45eb404d60583a3f57002c6eba36d914db06dcd
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-10-02 18:19:48 +0300

    Add files to directory

---
+++ internal-api/src/main/kotlin/com/sushkevych/internalapi/NatsSubject.kt
@@ -0,0 +1,15 @@
+package com.sushkevych.internalapi
+
+object NatsSubject {
+    private const val REQUEST_PREFIX = "com.sushkevych.securitydevices.input.request"
+
+    object Device {
+        private const val DEVICE_PREFIX = REQUEST_PREFIX + ".device"
+
+        const val CREATE = DEVICE_PREFIX + ".create"
+        const val GET_BY_ID = DEVICE_PREFIX + ".get_by_id"
+        const val GET_ALL = DEVICE_PREFIX + ".get_all"
+        const val UPDATE = DEVICE_PREFIX + ".update"
+        const val DELETE = DEVICE_PREFIX + ".delete"
+    }
+}
