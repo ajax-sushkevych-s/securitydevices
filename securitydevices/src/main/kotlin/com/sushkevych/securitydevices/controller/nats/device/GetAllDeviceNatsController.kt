fed167a4b019292ff8878e3bcb7917b2fb5ae8e2
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-10-03 11:10:55 +0300

    PETPROJECT-00008 Add NATS controllers Update NATS controllers

---
+++ securitydevices/src/main/kotlin/com/sushkevych/securitydevices/controller/nats/device/GetAllDeviceNatsController.kt
@@ -0,0 +1,41 @@
+package com.sushkevych.securitydevices.controller.nats.device
+
+import com.google.protobuf.Parser
+import com.sushkevych.internalapi.NatsSubject.Device.GET_ALL
+import com.sushkevych.securitydevices.commonmodels.device.Device
+import com.sushkevych.securitydevices.controller.nats.NatsController
+import com.sushkevych.securitydevices.dto.response.toDevice
+import com.sushkevych.securitydevices.input.request.device.get_all.proto.GetAllDeviceRequest
+import com.sushkevych.securitydevices.input.request.device.get_all.proto.GetAllDeviceResponse
+import com.sushkevych.securitydevices.service.DeviceService
+import io.nats.client.Connection
+import org.springframework.stereotype.Component
+
+@Component
+class GetAllDeviceNatsController(
+    override val connection: Connection,
+    private val deviceService: DeviceService
+) : NatsController<GetAllDeviceRequest, GetAllDeviceResponse> {
+
+    override val subject = GET_ALL
+    override val parser: Parser<GetAllDeviceRequest> = GetAllDeviceRequest.parser()
+
+    override fun handle(request: GetAllDeviceRequest): GetAllDeviceResponse = runCatching {
+        buildSuccessResponse(deviceService.getAllDevices().map { it.toDevice() })
+    }.getOrElse { exception ->
+        buildFailureResponse(exception.javaClass.simpleName, exception.toString())
+    }
+
+    private fun buildSuccessResponse(deviceList: List<Device>): GetAllDeviceResponse =
+        GetAllDeviceResponse.newBuilder().apply {
+            responseBuilder.successBuilder
+                .devicesBuilder
+                .addAllDevices(deviceList)
+        }.build()
+
+    private fun buildFailureResponse(exception: String, message: String): GetAllDeviceResponse =
+        GetAllDeviceResponse.newBuilder().apply {
+            responseBuilder.failureBuilder
+                .setMessage("Devices find failed by $exception: $message")
+        }.build()
+}
---
+++ securitydevices/src/main/kotlin/com/sushkevych/securitydevices/controller/nats/device/UpdateDeviceNatsController.kt
@@ -0,0 +1,45 @@
+package com.sushkevych.securitydevices.controller.nats.device
+
+import com.google.protobuf.Parser
+import com.sushkevych.internalapi.NatsSubject.Device.UPDATE
+import com.sushkevych.securitydevices.commonmodels.device.Device
+import com.sushkevych.securitydevices.controller.nats.NatsController
+import com.sushkevych.securitydevices.dto.request.toDeviceRequest
+import com.sushkevych.securitydevices.dto.response.toDevice
+import com.sushkevych.securitydevices.input.request.device.update.proto.UpdateDeviceRequest
+import com.sushkevych.securitydevices.input.request.device.update.proto.UpdateDeviceResponse
+import com.sushkevych.securitydevices.service.DeviceService
+import io.nats.client.Connection
+import org.springframework.stereotype.Component
+
+@Component
+class UpdateDeviceNatsController(
+    override val connection: Connection,
+    private val deviceService: DeviceService
+) : NatsController<UpdateDeviceRequest, UpdateDeviceResponse> {
+
+    override val subject = UPDATE
+    override val parser: Parser<UpdateDeviceRequest> = UpdateDeviceRequest.parser()
+
+    override fun handle(request: UpdateDeviceRequest): UpdateDeviceResponse = runCatching {
+        val device = request.request.device.toDeviceRequest()
+        val updatedDevice = device.id?.let { deviceId ->
+            deviceService.updateDevice(deviceId, device)
+        } ?: throw IllegalArgumentException("Device ID is null")
+        buildSuccessResponse(updatedDevice.toDevice())
+    }.getOrElse { exception ->
+        buildFailureResponse(exception.javaClass.simpleName, exception.toString())
+    }
+
+    private fun buildSuccessResponse(device: Device): UpdateDeviceResponse =
+        UpdateDeviceResponse.newBuilder().apply {
+            responseBuilder.successBuilder
+                .setDevice(device)
+        }.build()
+
+    private fun buildFailureResponse(exception: String, message: String): UpdateDeviceResponse =
+        UpdateDeviceResponse.newBuilder().apply {
+            responseBuilder.failureBuilder
+                .setMessage("Device update failed by $exception: $message")
+        }.build()
+}
