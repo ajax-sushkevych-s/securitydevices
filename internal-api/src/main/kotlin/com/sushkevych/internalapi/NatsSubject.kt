171c948ac9be2820029bc6f8e5d576f98e844e0b
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-10-07 17:22:00 +0300

    PETPROJECT-00009 Add update event for each device Update protobuf files due to conventions. Add Updated event proto message for Device. Update NATS subjects.

--- internal-api/src/main/kotlin/com/sushkevych/internalapi/NatsSubject.kt
+++ internal-api/src/main/kotlin/com/sushkevych/internalapi/NatsSubject.kt
@@ -3,13 +3,21 @@ package com.sushkevych.internalapi
 object NatsSubject {
     private const val REQUEST_PREFIX = "com.sushkevych.securitydevices.input.request"
 
-    object Device {
-        private const val DEVICE_PREFIX = REQUEST_PREFIX + ".device"
+    private const val EVENT_PREFIX = "com.sushkevych.securitydevices.output.pubsub"
 
-        const val CREATE = DEVICE_PREFIX + ".create"
-        const val GET_BY_ID = DEVICE_PREFIX + ".get_by_id"
-        const val GET_ALL = DEVICE_PREFIX + ".get_all"
-        const val UPDATE = DEVICE_PREFIX + ".update"
-        const val DELETE = DEVICE_PREFIX + ".delete"
+    object DeviceRequest {
+        private const val DEVICE_PREFIX = "$REQUEST_PREFIX.device"
+
+        const val CREATE = "$DEVICE_PREFIX.create"
+        const val GET_BY_ID = "$DEVICE_PREFIX.get_by_id"
+        const val GET_ALL = "$DEVICE_PREFIX.get_all"
+        const val UPDATE = "$DEVICE_PREFIX.update"
+        const val DELETE = "$DEVICE_PREFIX.delete"
+    }
+
+    object DeviceEvent {
+        private const val DEVICE_PREFIX = "$EVENT_PREFIX.device"
+
+        const val UPDATED = "$DEVICE_PREFIX.updated"
     }
 }
