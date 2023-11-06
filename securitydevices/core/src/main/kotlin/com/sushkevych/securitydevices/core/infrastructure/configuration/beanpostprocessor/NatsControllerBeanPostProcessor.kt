c511a8ce07df57bed011264e3b27e212da484438
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-11-06 12:21:16 +0200

    PETPROJECT-00015 hexagon and DDD Fix tests

--- securitydevices/core/src/main/kotlin/com/sushkevych/securitydevices/core/infrastructure/configuration/beanpostprocessor/NatsControllerBeanPostProcessor.kt
+++ securitydevices/core/src/main/kotlin/com/sushkevych/securitydevices/core/infrastructure/configuration/beanpostprocessor/NatsControllerBeanPostProcessor.kt
@@ -4,27 +4,26 @@ import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.stereotype.Component
 import com.google.protobuf.GeneratedMessageV3
 import com.sushkevych.securitydevices.core.infrastructure.adapters.nats.NatsController
-import io.nats.client.Connection
 import reactor.core.scheduler.Schedulers
 
 @Component
-class NatsControllerBeanPostProcessor(private val connection: Connection): BeanPostProcessor {
+class NatsControllerBeanPostProcessor: BeanPostProcessor {
 
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any {
         if (bean is NatsController<*, *>) {
-            initializeNatsController(bean, connection)
+            initializeNatsController(bean)
         }
         return bean
     }
 
     fun <RequestT : GeneratedMessageV3, ResponseT : GeneratedMessageV3>
-            initializeNatsController(controller: NatsController<RequestT, ResponseT>, connection: Connection) {
-        connection.createDispatcher { message ->
+            initializeNatsController(controller: NatsController<RequestT, ResponseT>) {
+        controller.connection.createDispatcher { message ->
             val parsedData = controller.parser.parseFrom(message.data)
             controller.handle(parsedData)
                 .map { it.toByteArray() }
                 .subscribeOn(Schedulers.boundedElastic())
-                .subscribe { connection.publish(message.replyTo, it) }
+                .subscribe { controller.connection.publish(message.replyTo, it) }
         }.apply { subscribe(controller.subject) }
     }
 }
