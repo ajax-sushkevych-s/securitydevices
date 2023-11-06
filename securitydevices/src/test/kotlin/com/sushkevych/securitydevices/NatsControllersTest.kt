91b553ab7fc12eefe1b6e0e77928f4ebdefc21f2
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-11-06 13:01:15 +0200

    PETPROJECT-00015 hexagon and DDD Fix tests

--- securitydevices/src/test/kotlin/com/sushkevych/securitydevices/NatsControllersTest.kt
+++ securitydevices/src/test/kotlin/com/sushkevych/securitydevices/NatsControllersTest.kt
@@ -233,7 +233,7 @@ class NatsControllersTest {
         val response = natsConnection.requestWithTimeout(
             subject,
             payload.toByteArray(),
-            Duration.ofSeconds(20L)
+            Duration.ofSeconds(5L)
         )
         return parser.parseFrom(response.get().data)
     }
