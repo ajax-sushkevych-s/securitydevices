cca9366bc3bffc51d15caffdb8868a4fc98e7716
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-11-17 11:56:04 +0200

    feature commit fix

--- securitydevices/user/build.gradle.kts
+++ securitydevices/user/build.gradle.kts
@@ -1,4 +1,4 @@
-dependencies { // feature commit
+dependencies { // feature commit // feature commit // extra fix //ffff
     implementation(project(":securitydevices:core"))// main 2nd commit
     implementation(project(":securitydevices:device-status"))// main 1st commit
 } // main 4th commit
