d2871f91352397ee5db9147acefa10abebf6e1d9
Serhii Sushkevych <sushkevych.s@ajax.systems>
2023-11-17 11:35:42 +0200

    main 1st

--- securitydevices/user/build.gradle.kts
+++ securitydevices/user/build.gradle.kts
@@ -1,4 +1,4 @@
-dependencies { // main 3rd commit
+dependencies { // feature commit
     implementation(project(":securitydevices:core"))// main 2nd commit
     implementation(project(":securitydevices:device-status"))// main 1st commit
 } // main 4th commit
